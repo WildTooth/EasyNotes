8f87198d1b9eca96c5cc843a98626a3899206308
WildTooth <champenv@outlook.dk>
2023-05-08 23:43:59 +0200

    Fixed small error in last commit

--- core/src/main/java/com/github/wildtooth/easynotes/EasyNotes.java
+++ core/src/main/java/com/github/wildtooth/easynotes/EasyNotes.java
@@ -2,7 +2,6 @@ package com.github.wildtooth.easynotes;
 
 import com.github.wildtooth.easynotes.commands.NoteCommand;
 import com.github.wildtooth.easynotes.configuration.EasyNotesConfiguration;
-import com.github.wildtooth.easynotes.listener.ExampleGameTickListener;
 import com.github.wildtooth.easynotes.managers.NoteManager;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
@@ -16,7 +15,6 @@ public class EasyNotes extends LabyAddon<EasyNotesConfiguration> {
 
     final NoteManager noteManager = new NoteManager();
 
-    this.registerListener(new ExampleGameTickListener(this));
     this.registerCommand(new NoteCommand(noteManager));
 
     this.logger().info("Enabled the Addon");
