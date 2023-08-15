7fd8ff29c5b6d5fb6662beac8218093d652dcc58
WildTooth <champenV@outlook.dk>
2023-08-15 09:22:19 +0200

    Removed NoteCommand.commandPath

--- core/src/main/java/com/github/wildtooth/easynotes/commands/note/NoteCommand.java
+++ core/src/main/java/com/github/wildtooth/easynotes/commands/note/NoteCommand.java
@@ -1,15 +1,13 @@
 package com.github.wildtooth.easynotes.commands.note;
 
+import com.github.wildtooth.easynotes.EasyNotes;
 import com.github.wildtooth.easynotes.managers.NoteManager;
 import net.labymod.api.client.chat.command.Command;
 import net.labymod.api.util.I18n;
 
 public class NoteCommand extends Command {
-  private static String commandPath;
-
   public NoteCommand(NoteManager noteManager, String langPath) {
     super("note", "n", "write");
-    commandPath = langPath + "command.";
     this.withSubCommand(new NoteViewCommand(noteManager, "view", "v"));
     this.withSubCommand(new NoteAddCommand(noteManager,"add", "a"));
     this.withSubCommand(new NoteRemoveCommand(noteManager, "remove", "r"));
@@ -18,11 +16,11 @@ public class NoteCommand extends Command {
 
   @Override
   public boolean execute(String prefix, String[] arguments) {
-    displayMessage(I18n.translate(commandPath + "unknown"));
+    displayMessage(I18n.translate(EasyNotes.getLangPath() + "command." + "unknown"));
     return true;
   }
 
   public static String commandUsage(String command) {
-    return I18n.translate(commandPath + command + ".usage");
+    return I18n.translate(EasyNotes.getLangPath() + "command." + command + ".usage");
   }
 }
